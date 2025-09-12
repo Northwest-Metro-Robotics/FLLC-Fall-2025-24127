61eb5345b44271eb00b386454704877cee5149b9
Brent M. Clark <brentmclark@gmail.com>
2025-09-12 16:58:02 -0500

    wip

--- src/hello_motor.py
+++ src/hello_motor.py
@@ -9,7 +9,8 @@ from constants import HUB, MOTOR_SPEED, MOTOR_TWO, WHEEL_ONE, WHEEL_TWO
 
 
 # to go forward and lunch the food out of the silo
-WHEEL_ONE.run_angle(MOTOR_SPEED,360)
-WHEEL_TWO.run_angle(MOTOR_SPEED,365)
-MOTOR_TWO.run_angle(MOTOR_SPEED,370)
+WHEEL_ONE.run_angle(MOTOR_SPEED,400)
+WHEEL_TWO.run_angle(MOTOR_SPEED,400)
+MOTOR_TWO.run_angle(MOTOR_SPEED,400)
 MOTOR_TWO.run_angle(MOTOR_SPEED,-370)
+WHEEL_TWO.run_angle(MOTOR_SPEED,1200)
