ed37c685b6da15b6f225d00959f7939750f4bfd7
Nasa2014 <ethanpereralougee@gmail.com>
2025-09-16 18:41:11 -0500

    Update test.py
    made it go forward 10 times

--- test.py
+++ test.py
@@ -1,5 +1,23 @@
 loop_counter = 1
+task_list = []
+
 while loop_counter <= 10:
-    (WHEEL_ONE.run_angle(MOTOR_SPEED,400)
-WHEEL_TWO.run_angle(MOTOR_SPEED,400))
-    loop_counter = loop_counter + 1
+    # Run motor actions
+    WHEEL_ONE.run_angle(MOTOR_SPEED, 400)
+    WHEEL_TWO.run_angle(MOTOR_SPEED, 400)
+    MOTOR_ONE.run_angle(MOTOR_SPEED, 600)
+    MOTOR_TWO.run_angle(MOTOR_SPEED, 600)
+
+    # Get user input
+    print("Enter a task:")
+    list_item = input().strip()  
+
+    if list_item == "":
+        print("You didn't enter a task.")
+    else:
+        task_list.append(list_item)
+        print("Current list:", task_list)
+
+    loop_counter += 1
+
+
