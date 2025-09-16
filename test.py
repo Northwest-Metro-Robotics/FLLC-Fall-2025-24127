4a4a8b52452a94f241f28849213ee6977f349684
Nasa2014 <ethanpereralougee@gmail.com>
2025-09-16 18:27:08 -0500

    Update test.py
    to make it go forward 10 times if I'm correct

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 loop_counter = 1
 while loop_counter <= 10:
-    print('hello')
+    (WHEEL_ONE.run_angle(MOTOR_SPEED,400)
+WHEEL_TWO.run_angle(MOTOR_SPEED,400))
     loop_counter = loop_counter + 1
